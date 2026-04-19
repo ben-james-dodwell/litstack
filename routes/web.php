ef4c2379783f274697d377e20e317f9022b80470
Ben James Dodwell <61091068+ben-james-dodwell@users.noreply.github.com>
2026-04-19 08:47:06 +0100

    Add POST debug route excluded from CSRF to diagnose nginx header forwarding

--- routes/web.php
+++ routes/web.php
@@ -4,6 +4,17 @@ use Illuminate\Support\Facades\Route;
 
 Route::view('/', 'welcome')->name('home');
 
+Route::post('/debug-post', function (\Illuminate\Http\Request $request) {
+    return response()->json([
+        'session_token'  => session()->token(),
+        'x_csrf_header'  => $request->header('X-CSRF-TOKEN'),
+        'content_type'   => $request->header('Content-Type'),
+        'is_json'        => $request->isJson(),
+        '_token_input'   => $request->input('_token'),
+        'raw_body_start' => substr($request->getContent(), 0, 100),
+    ]);
+});
+
 Route::get('/debug-headers', function (\Illuminate\Http\Request $request) {
     return response()->json([
         'session_token'   => session()->token(),
